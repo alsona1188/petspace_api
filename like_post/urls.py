76819fd17e32b5a23b32d3bb731e126aac1acc8a
Alsona Natsi <alsona.naci1188@gmail.com>
2024-09-08 23:56:45 +0000

    Handling duplicate likes by creating the create function in serializers

---
+++ like_post/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+#from posts import views
+
+#urlpatterns = [
+    #path('like_post/', views.LikePostList.as_view()),
+    #path('like_post/<int:pk>/', views.LikePostDetail.as_view()),
+#]
