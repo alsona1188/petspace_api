457c3f83e15c9edb14acaa8310e796a3b96f0d04
Alsona Natsi <alsona.naci1188@gmail.com>
2024-09-08 05:35:59 +0000

    Updating the serializer and add image validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from posts.models import Post
-from taggit.serializers import (TagListSerializerField, TaggitSerializer)
+from taggit.serializers import TagListSerializerField
+from posts.models import PostImage
 
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ['id', 'image']
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -9,6 +14,20 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     tags = TagListSerializerField()
+    images = PostImageSerializer(many=True, read_only=True)
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -19,5 +38,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'description', 'tags', 'category', 'images'
         ]
