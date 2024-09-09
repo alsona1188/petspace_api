9dbd71fad5cfcaabfae5d475dc9a31fc841475da
Alsona Natsi <alsona.naci1188@gmail.com>
2024-09-09 12:27:04 +0000

    Filtering posts and profiles with DjangoFilterBackend

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from petspace.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -16,9 +17,17 @@ class PostList(generics.ListCreateAPIView):
         likes_count=Count('like_post', distinct=True),
         comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
+
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'like_post__owner__profile',
+        'owner__profile',
     ]
     search_fields = [
         'owner__username',
@@ -27,6 +36,7 @@ class PostList(generics.ListCreateAPIView):
     ordering_fields = [
         'likes_count',
         'comments_count',
+        'likes__created_at',
     ]
 
     def perform_create(self, serializer):
